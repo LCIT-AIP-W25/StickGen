1987b1f70f6f7a9ff3c46e20d0621d5dcefd28a6
Dharmil290998 <150631303+Dharmil290998@users.noreply.github.com>
2025-04-10 12:00:38 -0400

    Add files via upload

---
+++ frontend/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewsSection from '../components/NewsSection';
+
+const Home = () => {
+    return (
+        <div className="home-page">
+            <NewsSection />
+        </div>
+    );
+};
+
+export default Home;
