eec51d4dcbb98de1b2b3ded97f131c85e674eb98
JISNA D KUNJU <your.new@email.com>
2025-07-10 11:17:24 -0400

    Implemented Stable diffusion model for sticker generation
    Implemented Stable diffusion model for sticker generation

--- Integration/modeling_service/mongo_writer.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["news_db"]
-collection = db["news_articles"]
-
-def save_to_mongo(article):
-    collection.insert_one(article)
-    print("🍃 Saved to MongoDB")
